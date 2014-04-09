c627511dccbc3a1d8aa3f8e715bb4eb15920c5d3
Carlos Sanchez <kusukumusu@gmail.com>
2014-04-09 11:29:02 +0200

    Cleaning the code. Version code 2.

--- L8VoiceRecognition/gen/com/l8smartlight/l8voicerecognition/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.l8smartlight.l8voicerecognition;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
